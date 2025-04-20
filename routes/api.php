bfa7f695db3f2c8ff6cd34922b4d6d5f96d88b7e
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-04-20 09:52:27 +0800

    Sign In and Sign out

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Jazer\Users\Http\Controllers\SignIn\EmailPassword;
+use Jazer\Users\Http\Controllers\SignOut\SignOut;
 
 
 Route::group(['prefix' => 'users'], function () {
-    Route::get('signin', [EmailPassword::class, 'signin']);
+    Route::get('signinEmailPass', [EmailPassword::class, 'signin']);
+    Route::get('signout/{token}', [SignOut::class, 'signout']);
 });
 
