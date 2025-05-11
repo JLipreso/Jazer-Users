580fbeb13c00d73739f4447070a3de3567630283
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-05-11 15:07:24 +0800

    Added api route

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,13 @@ use Jazer\Users\Http\Controllers\Fetch\Paginate;
 use Jazer\Users\Http\Controllers\Fetch\Single;
 use Jazer\Users\Http\Controllers\Delete\Delete;
 
-Route::group(['prefix' => 'users'], function () {
-    Route::get('signinEmailPass', [EmailPassword::class, 'signin']);
-    Route::get('signout/{token}', [SignOut::class, 'signout']);
-    Route::get('paginate', [Paginate::class, 'paginate']);
-    Route::get('single/{people_refid}', [Single::class, 'single']);
-    Route::get('delete/{people_refid}', [Delete::class, 'delete']);
+Route::group(['prefix' => 'api'], function () {
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('signinEmailPass', [EmailPassword::class, 'signin']);
+        Route::get('signout/{token}', [SignOut::class, 'signout']);
+        Route::get('paginate', [Paginate::class, 'paginate']);
+        Route::get('single/{people_refid}', [Single::class, 'single']);
+        Route::get('delete/{people_refid}', [Delete::class, 'delete']);
+    });
 });
 
