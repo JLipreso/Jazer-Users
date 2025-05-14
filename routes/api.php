2dbf11b270815f663c6936f7b17963d7b785a195
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-05-15 06:15:50 +0800

    Basic Registration added

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Jazer\Users\Http\Controllers\SignIn\EmailPassword;
-use Jazer\Users\Http\Controllers\SignOut\SignOut;
-use Jazer\Users\Http\Controllers\Fetch\Paginate;
-use Jazer\Users\Http\Controllers\Fetch\Single;
-use Jazer\Users\Http\Controllers\Delete\Delete;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'users'], function () {
-        Route::get('signinEmailPass', [EmailPassword::class, 'signin']);
-        Route::get('signout/{token}', [SignOut::class, 'signout']);
-        Route::get('paginate', [Paginate::class, 'paginate']);
-        Route::get('single/{people_refid}', [Single::class, 'single']);
-        Route::get('delete/{people_refid}', [Delete::class, 'delete']);
+        Route::get('signinEmailPass', [Jazer\Users\Http\Controllers\SignIn\EmailPassword::class, 'signin']);
+        Route::get('signout/{token}', [Jazer\Users\Http\Controllers\SignOut\SignOut::class, 'signout']);
+        Route::get('paginate', [Jazer\Users\Http\Controllers\Fetch\Paginate::class, 'paginate']);
+        Route::get('single/{people_refid}', [Jazer\Users\Http\Controllers\Fetch\Single::class, 'single']);
+        Route::get('delete/{people_refid}', [Jazer\Users\Http\Controllers\Delete\Delete::class, 'delete']);
+        Route::get('registerBasic', [Jazer\Users\Http\Controllers\Register\Register::class, 'basic']);
     });
 });
 
